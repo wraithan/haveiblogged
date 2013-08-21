0dd60559a8c34f96cd03e988315ab381f750c594
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2013-08-20 19:49:50 -0700

    remove jquery

--- static/js/get-status.js
+++ static/js/get-status.js
@@ -1,25 +1,47 @@
-var my_data = null
+function make_element(type, opt) {
+  var ele = document.createElement(type)
+  for (var key in opt) {
+    ele[key] = opt[key]
+  }
+  return ele
+}
 
-$(function() {
-  $.getJSON('/status.json', function(data) {
-    $('#header').append($('<small>', {text: ' (since ' + data.since + ')'}))
-    $.each(data.categories, function(cat, blogged) {
-      var block = $('<div>', {
-        class: 'col-lg-4 col-md-4 col-sm-12 col-xs-12'
+window.onload = function() {
+  var xhr = new XMLHttpRequest()
+  xhr.open('GET', '/status.json', true)
+  xhr.responseType = 'json'
+  xhr.onload = function(e) {
+    if (this.status === 200) {
+      var data = xhr.response
+      var since = make_element('small', {
+        textContent: ' (since ' + data.since + ')'
       })
-      var link = $('<a>', {
-        href: 'http://blog.wraithan.net/category/' + cat + '/'
-      , class: 'btn btn-block'
-      }).append($('<h2>', {
-        text: cat
-      }))
-      block.append(link)
-      if (blogged) {
-        link.addClass('btn-success')
-      } else {
-        link.addClass('btn-danger')
+      document.getElementById('header').appendChild(since)
+
+      for (var cat in data.categories) {
+        var block = make_element('div', {
+          className: 'col-lg-4 col-md-4 col-sm-12 col-xs-12'
+        })
+
+        var link = make_element('a', {
+          className: 'btn btn-block'
+        , href: 'http://blog.wraithan.net/category/' + cat + '/'
+        })
+
+        link.appendChild(make_element('h2', {
+          textContent: cat
+        }))
+
+        if (data.categories[cat]) {
+          link.className += ' btn-success'
+        } else {
+          link.className += ' btn-failure'
+        }
+
+        block.appendChild(link)
+        document.getElementById('status-display').appendChild(block)
       }
-      block.appendTo($('#status-display'))
-    })
-  })
-})
+    }
+  }
+  xhr.send()
+}
